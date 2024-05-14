d22fa769756d71ba20a8fb8423e77a39b07c69e2
kdeluz <kdeluz@rrc.ca>
2024-05-14 10:29:33 -0500

    Finished Part 1

--- carl_sagan.rb
+++ carl_sagan.rb
@@ -20,7 +20,7 @@ sagan = [
 
 # We are a way for the cosmos to know itself.
 
-puts "#{carl[:words][2]} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} "
+puts "#{carl[:words][2]} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}"
 
 # Here is an example of building a setence out of array/hash pieces.
 example = [ 'test', 'a', 'is']
